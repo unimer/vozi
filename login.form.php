b31507617649614c3812ecb73b40abcb3028429f
Nikola Marin <nikolamarin@protonmail.com>
2019-06-08 15:59:29 +0200

    login form

---
+++ login.form.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if IE 7]>                  <html class="ie7 no-js" lang="en">     <![endif]-->
+<!--[if lte IE 8]>              <html class="ie8 no-js" lang="en">     <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="not-ie no-js" lang="en">  <!--<![endif]-->
+    <head>
+
+        <!-- Basic Page Needs -->
+        <meta charset="utf-8">
+        <title>My ride - Rides</title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Styles -->
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Forms -->
+        <link href="css/jquery.idealforms.css" rel="stylesheet">
+        <!-- Select  -->
+        <link href="css/jquery.idealselect.css" rel="stylesheet">
+        <!-- Slicknav  -->
+        <link href="css/slicknav.css" rel="stylesheet">
+        <!-- Main style -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- Modernizr -->
+        <script src="js/modernizr.js"></script>
+
+        <!-- Fonts -->
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+
+<body>
+  <?php require "header.php" ?>
+</body>
+
+</html>
