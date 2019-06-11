407e6f05ea991ae36b568857cc62f7fa9151ebf0
bokas5 <47373988+bokas5@users.noreply.github.com>
2019-06-11 13:41:49 +0200

    click
    all div blocks are now clickable instead of separate small a hrefs

--- includes/list-rides.inc.php
+++ includes/list-rides.inc.php
@@ -10,31 +10,31 @@ $seats =$_POST ["seats"];
 function print_rides($data){
   while ($all = mysqli_fetch_assoc($data)) {
 
-
+                             echo "<a href=\"#\">";
                             echo "<article class=\"ride-box clearfix\">";
-
-                                echo "<div class=\"ride-content\">";
-                                    echo "<h3><a href=\"#\">".$all[startpoint]."-".$all[endpoint]."</a></h3>vozi: <a href=\"#\">".$all[usernameRides]."</a>";
+                        
+                                echo "<div class=\"ride-content\" >";
+                                    echo "<h3>".$all[startpoint]."-".$all[endpoint]."</h3>vozi: ".$all[usernameRides]."";
                                 echo "</div>";
-
+                                
                                 echo "<ul class=\"ride-meta\">";
 
                                     echo "<li class=\"ride-date\">";
-                                        echo "<a href=\"#\" class=\"tooltip-link\" data-original-title=\"Date\" data-toggle=\"tooltip\">";
+                                        // echo "<a href=\"#\" class=\"tooltip-link\" data-original-title=\"Date\" data-toggle=\"tooltip\">";
                                             //echo "<i class=\"fa fa-calendar\"></i>";
                                             echo $all[dateRides];
 
-                                        echo "</a>";
+                                       
                                     echo "</li>"; //<!-- end .ride-date -->
 
                                     echo "<li class=\"ride-times\">";
-                                      echo "<a href=\"#\" class=\"tooltip-link\" data-original-title=\"Date\" data-doggle=\"tooltip\">";
+                                      echo "<class=\"tooltip-link\" data-original-title=\"Date\" data-doggle=\"tooltip\">";
                                         echo $all[timeRides];
-                                      echo "</a>";
+                                     
                                     echo "</li>";
 
                                     echo "<li class=\"ride-people\">";
-                                        echo "<a href=\"#\" class=\"tooltip-link\" data-original-title=\"Number of seats\" data-toggle=\"tooltip\">";
+                                        echo "< class=\"tooltip-link\" data-original-title=\"Number of seats\" data-toggle=\"tooltip\">";
                                           //  echo "<i class=\"fa fa-user\"></i>";
                                           if ($all[seats] == 1) {
                                             echo $all[seats]. "     Mjesto";
@@ -43,18 +43,19 @@ function print_rides($data){
                                             echo $all[seats]."      Mjesta ";
                                           }
 
-                                        echo "</a>";
+                                      
                                     echo "</li>"; //"<!-- end .ride-people -->
 
                                     echo "<li class=\"ride-times\">";
-                                      echo "<a href=\"#\" class=\"tooltip-link\" data-original-title=\"Date\" data-doggle=\"tooltip\">";
+                                      echo "< class=\"tooltip-link\" data-original-title=\"Date\" data-doggle=\"tooltip\">";
                                         echo $all[cost]. " KM";
-                                      echo "</a>";
+                                     
                                     echo "</li>";
 
                                 echo "</ul>"; //"<!-- end .ride-meta -->
 
                             echo "</article>"; //"<!-- end .ride-box -->
+                            echo "</a>";
                     }
 
 }
